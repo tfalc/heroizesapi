14ab4da874c4e9217809242441754514631f4eb8
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-03-11 15:39:15 -0300

    Initial commit

---
+++ src/main/java/com/dio/marveu/heroizesapi/HeroizesapiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.marveu.heroizesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroizesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroizesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/marveu/heroizesapi/HeroizesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.marveu.heroizesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroizesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
